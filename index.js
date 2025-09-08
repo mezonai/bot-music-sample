f427ae250dc0b02ecfdb082af43b7feab04763a2
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2025-09-08 14:52:08 +0700

    update bot on prod

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ dotenv.config();
 const handlePlay = require("./commands/play");
 
 async function main() {
-  // const client = new MezonClient(process.env.APPLICATION_TOKEN); // Nếu bot trong Product thì dùng cái này
-  const client = new MezonClient(process.env.MEZON_TOKEN, process.env.HOST_DEV, process.env.PORT_DEV);
+  const client = new MezonClient(process.env.APPLICATION_TOKEN); // Nếu bot trong Product thì dùng cái này
+  // const client = new MezonClient(process.env.MEZON_TOKEN, process.env.HOST_DEV, process.env.PORT_DEV);
   const tokenObj = await client.login();
   
   client.onChannelMessage(async (event) => {
